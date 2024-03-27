6fdc3834fd1ac25539516d3eb01e569651da25ef
Manu254rce <straussmoabit@gmail.com>
2024-03-27 22:58:21 +0300

    Completed task 16

---
+++ 16-binary_tree_is_perfect.c
@@ -0,0 +1,24 @@
+# include "binary_trees.h"
+
+/**
+ * binary_tree_is_perfect - function that checks if a binary tree is perfect
+ * @tree: pointer to the root node of the tree to check
+ * Return: intger value showing that the tree is perfect
+ */
+
+int binary_tree_is_perfect(const binary_tree_t *tree)
+{
+	int depth = binary_tree_depth(tree);
+	int level = 0;
+
+	if (!tree)
+		return 0;
+
+	if (tree->left == NULL && tree->right == NULL)
+		return (depth == level + 1);
+
+	if (tree->left == NULL || tree->right == NULL)
+		return 0;
+
+	return (binary_tree_is_perfect(tree->left) && binary_tree_is_perfect(tree->right));
+}
