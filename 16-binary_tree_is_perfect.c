815c0bae765044c78b8130f594a680c937b862c1
Manu254rce <straussmoabit@gmail.com>
2024-03-27 23:23:58 +0300

    Completed task 17

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -12,13 +12,14 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 	int level = 0;
 
 	if (!tree)
-		return 0;
+		return (0);
 
 	if (tree->left == NULL && tree->right == NULL)
 		return (depth == level + 1);
 
 	if (tree->left == NULL || tree->right == NULL)
-		return 0;
+		return (0);
 
-	return (binary_tree_is_perfect(tree->left) && binary_tree_is_perfect(tree->right));
+	return (binary_tree_is_perfect(tree->left) &&
+			binary_tree_is_perfect(tree->right));
 }
