b6d417c876e7f72b8fb71d028226a79f265fdd1c
Manu254rce <straussmoabit@gmail.com>
2024-03-27 15:44:02 +0300

    Done task 9

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -24,13 +24,9 @@ size_t binary_tree_height(const binary_tree_t *tree)
 
 	if (tree == NULL)
 		return (0);
-	if (tree->left == NULL)
-		left_height = 0;
-	left_height = binary_tree_height(tree->left) + 1;
 
-	if (tree->right == NULL)
-		right_height = 0;
-	right_height = binary_tree_height(tree->right) + 1;
+	left_height = tree->left ? 1 + binary_tree_height(tree->left) : 0;
+	right_height = tree->right ? 1 + binary_tree_height(tree->right) : 0;
 
 	return (max(left_height, right_height));
 }
